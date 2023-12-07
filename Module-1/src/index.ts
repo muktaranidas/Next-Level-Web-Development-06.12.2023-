ad73c211afb32d545e6956a0b6199fba0bb8b03b
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-12-06 17:48:52 -0800

    primitive type in typescript

--- Module-1/src/index.ts
+++ Module-1/src/index.ts
@@ -1,2 +1,19 @@
-const course: string = "Next Level  Development";
-console.log(course);
+// let course: string = "Next Level  Development";
+// // console.log(course);
+// course = "Next Level Development";
+
+// let rollNumber: number = 123;
+// rollNumber = 345;
+// const isAdmin: boolean = true;
+let course:undefined;
+// course = 123;
+// course = "Mukta";
+// course = undefined;
+
+
+//Primitive Types
+// string
+// number
+// boolean
+// null
+// undefined
