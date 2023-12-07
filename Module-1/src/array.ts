51dff48dd142434bbfa8c4f714ebd25ffa9eb119
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-12-06 20:25:01 -0800

    object, literal types and optional types

--- Module-1/src/array.ts
+++ Module-1/src/array.ts
@@ -1,4 +1,5 @@
 const names = ['mukta', 'proma', 'oyeishi', 77 , true];
 // const rollNumbers: number[] = [55, 4, 88];
 
-const user:[number,string] = [2733, 'Mukta'];
+const user: [number, string] = [2733, 'Mukta'];
+
---
+++ Module-1/src/object.ts
@@ -0,0 +1,15 @@
+const user: {
+    readonly company:string,
+    name: string,
+    isMarried: boolean,
+    age: number,
+    husband?:string
+}
+    = {
+    company:"Programming Hero",
+    name: "Mukta",
+    age: 52,
+    isMarried: true,
+    // husband: "Sudipto"
+};
+// user.company = "PH Bangladesh";
