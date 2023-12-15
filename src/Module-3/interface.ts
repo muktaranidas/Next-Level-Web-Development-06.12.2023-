f153136c90d4c057a2bb62178e6a777fd23d2f80
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-12-15 14:20:25 -0800

    interface,type vs interface

--- src/Module-3/interface.ts
+++ src/Module-3/interface.ts
@@ -2,28 +2,46 @@ type User = {
     name: string;
     age: number;
 }
-type extendedUser = User &  {
-    role:string
-}
-interface IUser{
-    name: string;
-    age: number;
-}
-interface IExtendedUser extends IUser{
-    role:string
-}
-const userr: IExtendedUser = {
-    name: 'mukta',
-    age: 400,
-    role:'Unknown'
-}
-
 const userWithTypeAlias: User = {
     name: 'Type Alias',
     age: 1000,
 }
 
+type extendedUser = User &  {
+    role:string
+}
+
+
+
+interface IUser{
+    name: string;
+    age: number;
+}
 const userWithInterface: IUser = {
     name: 'Interface',
     age:200,
 }
+interface IExtendedUser extends IUser{
+    role:string
+}
+const userr: extendedUser = {
+    name: 'mukta',
+    age: 400,
+    role:'Unknown'
+}
+
+//object , function , array
+
+//function
+type addNumbersType = (num1: number, num2: number) => number
+// interface IAddNumbers {
+//     (num1:number, num2: number):number
+// }
+const addNumbers: addNumbersType = (num1, num2) => num1 + num2
+
+// array
+type rollNumbersType = number[]
+interface IRollNumbers{
+[index:number]:number
+}
+const rollNumbers: IRollNumbers = [1, 2, 3, 4]; //[index]
