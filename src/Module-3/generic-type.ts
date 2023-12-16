f9b1db49175eb819389b5dbb431b04c62152a2ed
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-12-15 18:32:32 -0800

    introduction of generic in type

---
+++ src/Module-3/generic-type.ts
@@ -0,0 +1,35 @@
+type GenericTuple<X, Y> = [X, Y]
+const relation: GenericTuple<string, string> = ['Mukta', 'Proma']
+type relationWithSalaryType = {name:string,salary:number}
+
+const relationWithSalary: GenericTuple<relationWithSalaryType, string> = [{
+    name: 'Mukta',
+    salary:100000
+},
+    'Mukta'
+]
+const relationWithSalary2: GenericTuple<relationWithSalaryType, string> = [{
+    name: "Thakur",
+    salary:100000
+},
+    'Mukta'
+]
+
+type GenericArray<T> = Array<T>
+
+const rollNumbers: GenericArray<number> = [44, 12, 4]
+const rollNumbers2: GenericArray<string> = ['44', '12', '4']
+const rollNumbers3: GenericArray<boolean> = [true, false]
+type NameRollType = { name: string, roll: number }
+
+const UserNameAndRollNumbers: GenericArray<NameRollType> = [
+    {
+    name: "Mukta",
+    roll:1
+    },
+    {
+    name: "Thakur",
+    roll: 2
+    }
+]
+
--- src/Module-3/interface.ts
+++ src/Module-3/interface.ts
@@ -1,47 +1,47 @@
-type User = {
-    name: string;
-    age: number;
-}
-const userWithTypeAlias: User = {
-    name: 'Type Alias',
-    age: 1000,
-}
-
-type extendedUser = User &  {
-    role:string
-}
-
-
-
-interface IUser{
-    name: string;
-    age: number;
-}
-const userWithInterface: IUser = {
-    name: 'Interface',
-    age:200,
-}
-interface IExtendedUser extends IUser{
-    role:string
-}
-const userr: extendedUser = {
-    name: 'mukta',
-    age: 400,
-    role:'Unknown'
-}
-
-//object , function , array
-
-//function
-type addNumbersType = (num1: number, num2: number) => number
-// interface IAddNumbers {
-//     (num1:number, num2: number):number
+// type User = {
+//     name: string;
+//     age: number;
 // }
-const addNumbers: addNumbersType = (num1, num2) => num1 + num2
+// const userWithTypeAlias: User = {
+//     name: 'Type Alias',
+//     age: 1000,
+// }
 
-// array
-type rollNumbersType = number[]
-interface IRollNumbers{
-[index:number]:number
-}
-const rollNumbers: IRollNumbers = [1, 2, 3, 4]; //[index]
+// type extendedUser = User &  {
+//     role:string
+// }
+
+
+
+// interface IUser{
+//     name: string;
+//     age: number;
+// }
+// const userWithInterface: IUser = {
+//     name: 'Interface',
+//     age:200,
+// }
+// interface IExtendedUser extends IUser{
+//     role:string
+// }
+// const userr: extendedUser = {
+//     name: 'mukta',
+//     age: 400,
+//     role:'Unknown'
+// }
+
+// //object , function , array
+
+// //function
+// type addNumbersType = (num1: number, num2: number) => number
+// // interface IAddNumbers {
+// //     (num1:number, num2: number):number
+// // }
+// const addNumbers: addNumbersType = (num1, num2) => num1 + num2
+
+// // array
+// type rollNumbersType = number[]
+// interface IRollNumbers{
+// [index:number]:number
+// }
+// const rollNumbers: IRollNumbers = [1, 2, 3, 4]; //[index]
