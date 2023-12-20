41e7647512d71624cf2c5a76ecf67d565d07d397
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-12-20 11:39:11 -0800

    conditional types

--- src/Module-3/asynchronous.ts
+++ src/Module-3/asynchronous.ts
@@ -1,81 +1,81 @@
-//Mocking
+// //Mocking
 
-// json place holder
-interface ITodo{
-    userId: number;
-    id: number;
-    title: string;
-    completed: boolean;
+// // json place holder
+// interface ITodo{
+//     userId: number;
+//     id: number;
+//     title: string;
+//     completed: boolean;
 
-}
-const getTodo = async ():Promise<ITodo> => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-   return await response.json()
-}
+// }
+// const getTodo = async ():Promise<ITodo> => {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+//    return await response.json()
+// }
 
-const getTodoData = async ():Promise<void> => {
-    const result = await getTodo()
-    console.log(result);
-}
-getTodoData()
-
-
+// const getTodoData = async ():Promise<void> => {
+//     const result = await getTodo()
+//     console.log(result);
+// }
+// getTodoData()
 
 
 
 
 
 
-const makePromise = ():Promise<string> => {
-    return new Promise<string>((resolve, reject) => {
-        const data: string = 'Data is fetche'
-        if (data) {
-            resolve(data)
-        }
-        else {
-            reject("Failed to fatch data!")
-        }
-    })
-}
-const makePromiseBoolean = ():Promise<boolean> => {
-    return new Promise<boolean>((resolve, reject) => {
-        const data: boolean = true
-        if (data) {
-            resolve(data)
-        }
-        else {
-            reject("Failed to fatch data!")
-        }
-    })
-}
-// type DataType = {
+
+
+// const makePromise = ():Promise<string> => {
+//     return new Promise<string>((resolve, reject) => {
+//         const data: string = 'Data is fetche'
+//         if (data) {
+//             resolve(data)
+//         }
+//         else {
+//             reject("Failed to fatch data!")
+//         }
+//     })
+// }
+// const makePromiseBoolean = ():Promise<boolean> => {
+//     return new Promise<boolean>((resolve, reject) => {
+//         const data: boolean = true
+//         if (data) {
+//             resolve(data)
+//         }
+//         else {
+//             reject("Failed to fatch data!")
+//         }
+//     })
+// }
+// // type DataType = {
+// //     data:string
+// // }
+// interface DataType  {
 //     data:string
 // }
-interface DataType  {
-    data:string
-}
-const makePromiseObject = ():Promise<DataType> => {
-    return new Promise<DataType>((resolve, reject) => {
-        const data: DataType = {data: "Data is Fetched"}
-        if (data) {
-            resolve(data)
-        }
-        else {
-            reject("Failed to fatch data!")
-        }
-    })
-}
-const getPromiseData = async ():Promise<string> => {
-    const data = await makePromise()
-    return data
-}
-const getPromiseDataBoolean = async ():Promise<boolean> => {
-    const data = await makePromiseBoolean()
-    return data
-}
-const getPromiseDataObject= async ():Promise<DataType> => {
-    const data = await getPromiseDataObject()
-    return data
-}
+// const makePromiseObject = ():Promise<DataType> => {
+//     return new Promise<DataType>((resolve, reject) => {
+//         const data: DataType = {data: "Data is Fetched"}
+//         if (data) {
+//             resolve(data)
+//         }
+//         else {
+//             reject("Failed to fatch data!")
+//         }
+//     })
+// }
+// const getPromiseData = async ():Promise<string> => {
+//     const data = await makePromise()
+//     return data
+// }
+// const getPromiseDataBoolean = async ():Promise<boolean> => {
+//     const data = await makePromiseBoolean()
+//     return data
+// }
+// const getPromiseDataObject= async ():Promise<DataType> => {
+//     const data = await getPromiseDataObject()
+//     return data
+// }
 
-//Promise<string> Promise<boolean> Promise<object> 
+// //Promise<string> Promise<boolean> Promise<object> 
---
+++ src/Module-3/conditional-type.ts
@@ -0,0 +1,31 @@
+// a type is dependent on another type
+
+type a1 = number;
+type a3 = undefined
+type a4 = number
+type a2 = a1 extends string ? string : null
+
+// nesed conditional type
+type d = a1 extends null? null: a3 extends number ? number : a4 extends null ? null:never
+
+
+type Sheikh = {
+    wife1: string
+    wife2:string
+}
+type A = keyof Sheikh  //'wife1' | 'wife2'
+                          //wife1 extends 'wife1' | 'wife2'
+type CheckProperty<T,K> = K extends keyof  Sheikh ? true : false
+type CheckWife2 = CheckProperty<Sheikh,'wife2'>
+
+// check korbe ei sheikh type a wife1 ashe kina ?  true:false
+// check korbe ei sheikh type a wife2 ashe kina ?  true:false
+// check korbe ei sheikh type a wife2 ashe kina ?  true:false
+
+
+
+
+// matha nosto example
+type Bandhubi = 'Proma' | 'Oyeishi' | 'Nazmin'
+type RemoveBandhubi<T,K> = T extends K? never : T
+type CurrentBandhubi = RemoveBandhubi<Bandhubi,'Oyeishi'>
