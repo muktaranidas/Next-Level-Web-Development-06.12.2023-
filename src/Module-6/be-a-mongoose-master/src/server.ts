2c0981acd3948622aeafc995b29eed6556e1d5e2
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-12-20 21:52:43 -0800

    be a mongoose master

---
+++ src/Module-6/be-a-mongoose-master/src/server.ts
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const app = express()
+const port = 5000
+//database connection
+async function bootstrap() {
+    try {
+      await mongoose.connect('mongodb://127.0.0.1:27017/practice-mongoose');
+  console.log(`Database connection successfull`);
+    } catch (err) {
+        console.log(`Failed to connect database`, err);
+  }
+}
+bootstrap()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Server is  listening on port ${port}`)
+})
