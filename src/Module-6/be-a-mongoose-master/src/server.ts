8d687b7d3b4daf143d008ffae6b740b0d972c1b3
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-12-22 19:10:18 -0800

    check only

--- src/Module-6/be-a-mongoose-master/src/server.ts
+++ src/Module-6/be-a-mongoose-master/src/server.ts
@@ -1,22 +1,24 @@
-const express = require('express')
-const mongoose = require('mongoose');
-const app = express()
+import express from 'express'
+// const express = require('express')
+import mongoose from 'mongoose'
+// const mongoose = require('mongoose');
+const app = express()+gggg
 const port = 5000
 //database connection
 async function bootstrap() {
     try {
       await mongoose.connect('mongodb://127.0.0.1:27017/practice-mongoose');
-  console.log(`Database connection successfull`);
+      console.log(`Database connection successfull`);
+
+
+      app.listen(port, () => {
+      console.log(`Server is  listening on port ${port}`)
+})
     } catch (err) {
         console.log(`Failed to connect database`, err);
   }
 }
 bootstrap()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Server is  listening on port ${port}`)
-})
+
