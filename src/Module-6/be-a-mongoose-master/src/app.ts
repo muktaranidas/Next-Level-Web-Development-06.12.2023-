008ee9e4aa69016cda33753f8da7a1d6fc5b3132
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-12-26 19:13:10 -0800

    how to relate interface and schema

--- src/Module-6/be-a-mongoose-master/src/app.ts
+++ src/Module-6/be-a-mongoose-master/src/app.ts
@@ -1,10 +1,106 @@
 import express, { Application,NextFunction,Request,Response } from 'express';
 import cors from "cors";
-const app:Application = express();
+import { Schema,model } from "mongoose";
+const app: Application = express();
+
 //using cors
 app.use(cors());
-app.get("/", (req:Request, res:Response, next:NextFunction) => {
-    res.send("Hello World");
-    next()
+// parse data
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+    //inserting a test data into mongodb
+    /*
+    step1: Interface
+    step2: Schema
+    step3: Model
+    step4: Database Query
+    */
+    // creating an interface
+    interface IUser {
+        id: string;
+        role: "student";
+        password: string;
+        name: {
+            firstName: string;
+            middleName?: string;
+            lastName: string;
+        };
+        dateOfBirth?: string;
+        gender: "male" | "female";
+        email?: string;
+        contactNo: string;
+        emergencyContactNo: string;
+        presentAddress: string;
+        permanentAddress: string;
+
+    }
+    // creating schema using interface
+    const userSchema = new Schema<IUser>({
+        id: {
+            type: String,
+            required: true,
+            unique:true,
+        },
+        role: {
+            type: String,
+            required: true,
+          
+        },
+        password: {
+            type: String,
+            required: true,
+            
+        },
+        name: {
+            firstName: {
+                type: String,
+                required: true,
+            },
+            middleName: {
+                type: String,
+                
+            },
+            lastName: {
+                type: String,
+                required: true,
+            },
+            
+        },
+        dateOfBirth: {
+            type:String,
+        },
+        gender: {
+            type: String,
+            enum:['male', 'female'],
+
+        },
+        email: {
+            type: String,
+            required:true,
+        },
+        contactNo: {
+            type: String,
+            required:true,
+        },
+        emergencyContactNo: {
+            type: String,
+            required:true,
+        },
+        presentAddress: {
+            type: String,
+            required:true,
+        },
+        permanentAddress: {
+            type: String,
+            required:true,
+        },
+
+  
 });
+
+
+    // res.send("Hello World");
+    // next()
+})
 export default app;
