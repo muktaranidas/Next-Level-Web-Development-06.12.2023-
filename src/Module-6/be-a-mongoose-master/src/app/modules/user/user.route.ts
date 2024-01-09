63a38ec7c1b66356145043c15c70585b17c821ce
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2024-01-08 19:28:56 -0800

    create and test post route , refactor code

--- src/Module-6/be-a-mongoose-master/src/app/modules/user/user.route.ts
+++ src/Module-6/be-a-mongoose-master/src/app/modules/user/user.route.ts
@@ -1,5 +1,7 @@
 import express from "express";
-import { createUser } from "./user.controller";
-const router = express.Router()
-router.get('/', createUser);
+import { createUser, getUsers } from "./user.controller";
+
+const router = express.Router();
+router.get('/', getUsers);
+router.post('/create-user', createUser);
 export default router;
--- src/Module-6/be-a-mongoose-master/src/app/modules/user/user.service.ts
+++ src/Module-6/be-a-mongoose-master/src/app/modules/user/user.service.ts
@@ -1,26 +1,13 @@
+import { IUser } from "./user.interface";
 import User from "./user.model";
-
- export const createUserToDB = async () => {
-        const user = await new User({
-            id: '890055',
-            role: "student",
-            // password: "Jhakanaka",
-            name: {
-                firstName: "Mukta Test Test",
-                middleName: "Thakur",
-                lastName: "Meghla",
-            },
-            password:'AmiPasswordJani77777',
-            // dateOfBirth?: "07.04.1998",
-            gender: "male",
-            email: "mukta.meghla123@gmail.com",
-            contactNo: "01302564259",
-            emergencyContactNo: "018000000",
-            presentAddress: "Dhaka",
-            permanentAddress: "Kuti",
-
-        });
-        await user.save();
-     // console.log(user);
-     return user
-    } 
+export const createUserToDB = async (payload:IUser): Promise<IUser> => {
+    const user = await new User(payload)
+        
+    await user.save();
+    // console.log(user);
+    return user
+};
+export const getUsersFromDB =async (): Promise<IUser[]> => {
+    const users = await User.find();
+    return users
+}
