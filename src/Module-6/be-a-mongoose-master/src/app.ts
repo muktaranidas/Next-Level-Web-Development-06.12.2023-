7838012292f00d5fc32a823d0df82e6768566504
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2024-01-08 18:02:20 -0800

    done

--- src/Module-6/be-a-mongoose-master/src/app.ts
+++ src/Module-6/be-a-mongoose-master/src/app.ts
@@ -17,86 +17,9 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
     step4: Database Query
     */
     // creating an interface
-    interface IUser {
-        id: string;
-        role: "student";
-        password: string;
-        name: {
-            firstName: string;
-            middleName?: string;
-            lastName: string;
-        };
-        dateOfBirth?: string;
-        gender: "male" | "female";
-        email?: string;
-        contactNo: string;
-        emergencyContactNo: string;
-        presentAddress: string;
-        permanentAddress: string;
-
-    }
+   
     // creating schema using interface
-    const userSchema = new Schema<IUser>({
-        id: {
-            type: String,
-            required: true,
-            unique:true,
-        },
-        role: {
-            type: String,
-            required: true,
-          
-        },
-        password: {
-            type: String,
-            required: true,
-            
-        },
-        name: {
-            firstName: {
-                type: String,
-                required: true,
-            },
-            middleName: {
-                type: String,
-                
-            },
-            lastName: {
-                type: String,
-                required: true,
-            },
-            
-        },
-        dateOfBirth: {
-            type:String,
-        },
-        gender: {
-            type: String,
-            enum:['male', 'female'],
-
-        },
-        email: {
-            type: String,
-            required:true,
-        },
-        contactNo: {
-            type: String,
-            required:true,
-        },
-        emergencyContactNo: {
-            type: String,
-            required:true,
-        },
-        presentAddress: {
-            type: String,
-            required:true,
-        },
-        permanentAddress: {
-            type: String,
-            required:true,
-        },
-    });
-    const User = model<IUser>('User', userSchema);
+   
     const createUserToDB = async () => {
         const user = new User({
             id: '777',
@@ -125,3 +48,12 @@ export default app;
 
 
 // pattern MVC , Modular
+
+/*
+Interface  -> interface.ts
+Schema , Model -> medel.ts
+route
+route function -> controler.ts
+Database Query Function --> service
+
+*/
---
+++ src/Module-6/be-a-mongoose-master/src/app/modules/user/user.model.ts
@@ -0,0 +1,65 @@
+import { Schema, model } from "mongoose";
+import { IUser } from "./user.interface";
+
+ const userSchema = new Schema<IUser>({
+        id: {
+            type: String,
+            required: true,
+            unique:true,
+        },
+        role: {
+            type: String,
+            required: true,
+          
+        },
+        password: {
+            type: String,
+            required: true,
+            
+        },
+        name: {
+            firstName: {
+                type: String,
+                required: true,
+            },
+            middleName: {
+                type: String,
+                
+            },
+            lastName: {
+                type: String,
+                required: true,
+            },
+            
+        },
+        dateOfBirth: {
+            type:String,
+        },
+        gender: {
+            type: String,
+            enum:['male', 'female'],
+
+        },
+        email: {
+            type: String,
+            required:true,
+        },
+        contactNo: {
+            type: String,
+            required:true,
+        },
+        emergencyContactNo: {
+            type: String,
+            required:true,
+        },
+        presentAddress: {
+            type: String,
+            required:true,
+        },
+        permanentAddress: {
+            type: String,
+            required:true,
+        },
+    });
+    const User = model<IUser>('User', userSchema);
+    
