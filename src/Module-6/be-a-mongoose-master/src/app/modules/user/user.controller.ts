329d8c0ac835b780e0d8b0745f606bf68d39cb79
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2024-01-08 20:22:57 -0800

    Find, FindOne, Field Filtering

--- src/Module-6/be-a-mongoose-master/src/app/modules/user/user.controller.ts
+++ src/Module-6/be-a-mongoose-master/src/app/modules/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { createUserToDB, getUsersFromDB } from "./user.service";
+import { createUserToDB, getUserByIdFromDb, getUsersFromDB } from "./user.service";
 
 export const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const data = req.body;
@@ -16,6 +16,15 @@ export const getUsers =async (req: Request, res: Response, next: NextFunction) =
       data:user ,
     })
 }
+export const getUserById = async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const user = await getUserByIdFromDb(id)
+    res.status(200).json({
+      status: 'success',
+      data:user ,
+    })
+}
+
 
 // Pattern
 //Route -> controller -> service
